a0ff6cdbd89899ceaa8ce0ce04364bd9e1b99a0c
siddhighodake25 <4782574@mitacsc.edu.in>
2024-10-09 11:57:03 +0530

    Update Addition.sol

--- Addition.sol
+++ Addition.sol
@@ -1,1 +1,9 @@
+def add_numbers(num1, num2):
+    return num1 + num2
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = add_numbers(num1, num2)
+
+print("The sum is:", result)
